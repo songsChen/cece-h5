9384e53e6e9ffbda960c3c8b9f5e3d93390435ac
chenshaosong <chenshaosong@dev02.aplum-inc.com>
2021-03-17 19:22:06 +0800

    tz

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,37 @@
 <template>
     <div class="about">
-        <h1>This is an about page</h1>
+        <div id="svgClass"></div>
     </div>
 </template>
 <script>
+import SVGA from "svgaplayerweb";
 export default {
+    data() {
+        return {};
+    },
     mounted() {
         console.log(this.$route.query);
+        this.playSvga();
+    },
+    methods: {
+        playSvga() {
+            const player = new SVGA.Player("#svgClass");
+            var parser = new SVGA.Parser("#svgClass");
+            //注意，这里必须是服务器地址，否则报错
+            parser.load(
+                "https://xxwolo.xxwolo.com/p_engine/apph5/activity/reports_year/loading.svga",
+                function (videoItem) {
+                    player.setVideoItem(videoItem);
+                    player.startAnimation();
+                }
+            );
+        },
     },
 };
 </script>
+<style scoped lang="less">
+#svgClass {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
